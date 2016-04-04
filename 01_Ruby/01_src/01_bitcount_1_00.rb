e28b591a1f17f63cb7fcf0ab391628dbeb043d1b
yuukubo <i.yusuke.shk@gmail.com>
2016-04-04 14:57:24 +0000

    ver up. validation added.

---
+++ 01_Ruby/01_src/01_bitcount_1_00.rb
@@ -0,0 +1,36 @@
+require "readline"
+
+inputnum = Readline.readline("type a number. > ")
+unless inputnum =~ /^[0-9]+$/
+  puts "  type a number. try again! "
+  sleep(2)
+  exit(99)
+end
+puts "count #{inputnum}(decimal number)'s bit.\n\n"
+ctr = 0
+digwei = []
+sumnum = 0
+inputnum = inputnum.to_i
+
+# 十進数を二進数の重みに分解パート
+while inputnum > 0
+  if inputnum < 2 ** ctr
+    inputnum -= 2 ** ( ctr - 1 )
+    digwei << ( ctr - 1 )
+    ctr = 0
+  elsif inputnum == 2 ** ctr
+    inputnum -= 2 ** ctr
+    digwei << ctr
+    ctr = 0
+  else
+    ctr += 1
+  end
+end
+
+# 記録した重みから二進数作成パート
+while digwei.length > 0
+    sumnum += 10 ** digwei.shift
+end
+
+puts "#{sumnum}(binary number)"
+puts "#{sumnum.to_s.size} bit"
